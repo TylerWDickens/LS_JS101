711de58000a54c32402f92e663a50f3a9efded68
Tyler Dickens <tyler.william.dickens@gmail.com>
2021-06-23 19:06:08 -0700

    enclosed in function and checked against ESLint

--- loan_calculator/loan_calculator.js
+++ loan_calculator/loan_calculator.js
@@ -1,87 +1,110 @@
+/* eslint-disable no-console */
 const readline = require('readline-sync');
 const MESSAGES = require('./loan_messages.json');
-//intRate and loan amount are in months
-
+//  intRate and loan amount are in months
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
 function paymentCalculator(amount, rate, duration) {
-  //convert percent rate to decimal and convert to monthly rate
+  //  convert percent rate to decimal and convert to monthly rate
   if (rate === 0) {
     return amount / duration;
-  } else {
-    let payment = amount * (rate / (1 - Math.pow((1 + rate), (-duration))));
-    return payment.toFixed(2);
   }
+  const payment = amount * (rate / (1 - (1 + rate) ** (-duration)));
+  return payment.toFixed(2);
 }
 
 function loanRate(unit, rate) {
-  switch(unit){
+  let monthRate;
+  let newUnit;
+  switch (unit) {
     case 'y':
-      return rate / (12 * 100);
+      monthRate = rate / (12 * 100);
       break;
     case 'm':
-      return rate / 100;
+      monthRate = rate / 100;
       break;
     default:
-      prompt(MESSAGES['valid_unit']);
-      let newUnit = readline.question();
+      prompt(MESSAGES.valid_unit);
+      newUnit = readline.question();
       loanRate(newUnit, rate);
+      break;
   }
+  return monthRate;
 }
 
 function invalidNumber(number) {
-  return  (number.trimStart()) === '' ||Number.isNaN(Number(number));
+  return (number.trimStart()) === '' || Number.isNaN(Number(number));
 }
 
 function loanMonths(unit, duration) {
-  switch(unit){
+  let months;
+  switch (unit) {
     case 'y':
-      return duration * 12;
+      months = duration * 12;
       break;
     case 'm':
-      return duration;
+      months = duration;
       break;
+    default:
+      break;
+  }
+  return months;
+}
+
+function loanCalculator() {
+  console.log(MESSAGES.welcome);
+  prompt(MESSAGES.loan_amount);
+  let loanAmount = readline.question();
+
+  while (invalidNumber(loanAmount)) {
+    prompt(MESSAGES.valid_number);
+    loanAmount = readline.question();
+  }
+
+  prompt(MESSAGES.APR);
+  let intRate = readline.question();
+
+  while (invalidNumber(intRate)) {
+    prompt(MESSAGES.valid_number);
+    intRate = readline.question();
+  }
+
+  // Check if the user wants to enter the duration in years or months
+  prompt(MESSAGES.loan_unit);
+  let loanUnit = readline.question();
+
+  while (!['y', 'm'].includes(loanUnit)) {
+    prompt(MESSAGES.valid_unit);
+    loanUnit = readline.question();
+  }
+
+  prompt(MESSAGES.loan_duration);
+  let loanDuration = readline.question();
+
+  while (invalidNumber(loanDuration)) {
+    prompt(MESSAGES.valid_number);
+    loanDuration = readline.question();
+  }
+
+  const rate = loanRate(loanUnit, intRate);
+  const months = loanMonths(loanUnit, loanDuration);
+  const monthlyPayment = paymentCalculator(loanAmount, rate, months);
+
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  prompt(`${MESSAGES['monthly payment']} $${monthlyPayment}`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+
+  prompt(MESSAGES.loop);
+  const loop = readline.question();
+
+  if (loop === 'y') {
+    loanCalculator();
+  } else if (loop === 'n') {
+    prompt(MESSAGES.goodbye);
   }
 }
 
-//Need to get loan amount, annual percentage rate (APR) and loan duration
-console.log(MESSAGES['welcome']);
-prompt(MESSAGES['loan_amount'])
-let loanAmount = readline.question();
-
-while(invalidNumber(loanAmount)) {
-  prompt(MESSAGES['valid_number']);
-  loanAmount = readline.question();
-}
-
-prompt(MESSAGES['APR'])
-let intRate = readline.question();
-
-while(invalidNumber(intRate)) {
-  prompt(MESSAGES['valid_number']);
-  intRate = readline.question();
-}
-
-//Check if the user wants to enter the duration in years or months
-prompt(MESSAGES['loan_unit'])
-let loanUnit = readline.question();
-
-while (!['y','m'].includes(loanUnit)) {
-  prompt(MESSAGES['valid_unit']);
-  operation = readline.question();
-}
-
-prompt(MESSAGES['loan_duration'])
-let loanDuration = readline.question();
-
-while(invalidNumber(loanDuration)) {
-  prompt(MESSAGES['valid_number']);
-  loanDuration = readline.question();
-}
-
-let monthlyPayment = paymentCalculator(loanAmount, loanRate(loanUnit, intRate), loanMonths(loanUnit, loanDuration));
-
-prompt(`${MESSAGES['monthly payment']} $${monthlyPayment}`);
+loanCalculator();
