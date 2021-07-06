d8e970d8d8200649d6e30c1826f7d534112c4c4a
Tyler Dickens <tyler.william.dickens@gmail.com>
2021-07-05 21:55:58 -0700

    added functions for scorekeeping, and shrthand

--- rock_paper_scissors/rock_paper_scissors.js
+++ rock_paper_scissors/rock_paper_scissors.js
@@ -1,5 +1,6 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"];
+const SHORTHAND_CHOICES = ["rock (r)", "paper (p)", "scissors (s)", "lizard (l)", "spock (o)"];
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
@@ -11,16 +12,81 @@ function displayWinner(choice, computerChoice) {
       (choice === 'scissors' && computerChoice === 'paper') ||
       (choice === 'lizard' && computerChoice === 'spock') ||
       (choice === 'rock' && computerChoice === 'lizard') ||
-      (choice === 'spock' && computerChoice === 'scissors') ||) {
+      (choice === 'spock' && computerChoice === 'scissors') ||
+      (choice === 'scissors' && computerChoice === 'lizard') ||
+      (choice === 'lizard' && computerChoice === 'paper') ||
+      (choice === 'paper' && computerChoice === 'spock') ||
+      (choice === 'spock' && computerChoice === 'rock')) {
     prompt('You win!');
-    userScore += 1;
+    scoreCount(1,0);
   } else if ((choice === 'rock' && computerChoice === 'paper') ||
             (choice === 'paper' && computerChoice === 'scissors') ||
-            (choice === 'scissors' && computerChoice === 'rock')) {
+            (choice === 'scissors' && computerChoice === 'rock') ||
+            (choice === 'spock' && computerChoice === 'lizard') ||
+            (choice === 'lizard' && computerChoice === 'rock') ||
+            (choice === 'scissors' && computerChoice === 'spock') ||
+            (choice === 'lizard' && computerChoice === 'scissors') ||
+            (choice === 'paper' && computerChoice === 'lizard') ||
+            (choice === 'spock' && computerChoice === 'paper') ||
+            (choice === 'rock' && computerChoice === 'spock')) {
     prompt('Computer wins!');
-    computerScore += 1;
+    scoreCount(0,1);
   } else {
     prompt("It's a tie");
+  }
+}
+
+function scoreCount(userPoint,computerPoint) {
+  if (userPoint === 1) {
+    userScore += 1;
+  } else if (computerPoint === 1) {
+    computerScore += 1;
+  }
+}
+
+function playAgain() {
+  prompt('Do you want to play again (y/n)?');
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt(`Please enter "y" or "n".`)
+    answer = readline.question().toLowerCase();
+  }
+  return answer;
+}
+
+function scoreKeeper(user, computer) {
+  if (user === 5 || computer === 5) {
+    if (user === 5) {
+      prompt(`User wins! Final score is User: ${userScore} Computer: ${computerScore}`)
+    } else if (computer === 5) {
+      prompt(`Computer wins! Final score is User: ${userScore} Computer: ${computerScore}`)
+    }
+    userScore = 0;
+    computerScore = 0;
+    return playAgain() === 'y'? true : false;
+  }
+    return true;
+}
+
+function choiceConversion(input) {
+  switch(input) {
+    case "r":
+      return "rock"
+      break;
+    case "p":
+      return "paper";
+      break;
+    case "s":
+      return "scissors";
+      break;
+    case "l":
+      return "lizard";
+      break;
+    case "o":
+      return "spock";
+      break;
+    default:
+      break;
   }
 }
 
@@ -28,13 +94,13 @@ let userScore = 0;
 let computerScore = 0;
 
 while (true) {
-  prompt (`Choose one: ${VALID_CHOICES.join(", ")}`);
-  let choice = readline.question();
+  prompt (`Choose one: ${SHORTHAND_CHOICES.join(", ")}`);
+  let userChoice = readline.question();
+  choice = choiceConversion(userChoice);
 
   while(!VALID_CHOICES.includes(choice)) {
     prompt(`Enter one of ${VALID_CHOICES.join(", ")}:`)
     choice = readline.question();
-
   }
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
@@ -46,12 +112,5 @@ while (true) {
 
   prompt(`Current score is User: ${userScore} Computer: ${computerScore}`);
 
-  prompt('Do you want to play again (y/n)?');
-  let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt(`Please enter "y" or "n".`)
-    answer = readline.question().toLowerCase();
-  }
-
-  if (answer[0] !== 'y') break;
+  if (scoreKeeper(userScore, computerScore) === false) break;
 }
