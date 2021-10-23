55da250e3a9e2a565515a80389d17c9c66ed5603
Tyler Dickens <tyler.william.dickens@gmail.com>
2021-10-23 16:48:35 -0700

    refactor code and run through eslint

--- lesson_2/calculator/refresher/calculator2.js
+++ lesson_2/calculator/refresher/calculator2.js
@@ -1,41 +1,59 @@
-const readline = require("readline-sync");
+const readline = require('readline-sync');
 
-//Ask the user for the first number
-//Ask the user for the second number
-//Ask the user for an operation to perform.
-//Perform the operation on the two numbers.
-//Print the result to the terminal
+function prompt(message) {
+  // eslint-disable-next-line no-console
+  console.log(`=> ${message}`);
+}
 
-console.log("Welcome to Calculator");
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
 
-console.log("Whats the first number?");
-const number1 = readline.question();
+prompt('Welcome to Calculator');
 
-console.log("Whats the second number?");
-const number2 = readline.question();
+prompt('Whats the first number?');
+let number1 = readline.question();
 
-console.log(
-  "What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide"
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
+prompt('Whats the second number?');
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number2 = readline.question();
+}
+
+prompt(
+  'What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide',
 );
 let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3, or 4:');
+  operation = readline.question();
+}
 
 let output;
 
 switch (operation) {
-  case "1":
+  case '1':
     output = Number(number1) + Number(number2);
     break;
-  case "2":
+  case '2':
     output = Number(number1) - Number(number2);
     break;
-  case "3":
+  case '3':
     output = Number(number1) * Number(number2);
     break;
-  case "4":
+  case '4':
     output = Number(number1) / Number(number2);
     break;
   default:
-    console.log("Please input a valid operator number");
+    prompt('Please input a valid operator number');
 }
 
-console.log(`The result is ${output}`);
+prompt(`The result is ${output}`);
