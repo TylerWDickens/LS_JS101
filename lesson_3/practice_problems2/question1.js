4b1804a3fb6de2470d4efbaeeccf820fd314eedf
Tyler Dickens <tyler.william.dickens@gmail.com>
2021-10-23 16:27:21 -0700

    old commits

--- lesson_3/practice_problems2/question1.js
+++ lesson_3/practice_problems2/question1.js
@@ -1,4 +1,5 @@
-let advice = "Few things in life are as important as house training your pet dinosaur.";
+let advice =
+  "Few things in life are as important as house training your pet dinosaur.";
 let newAdvice = advice.replace("important", "urgent");
 
 console.log(newAdvice);
--- lesson_3/practice_problems2/question4.js
+++ lesson_3/practice_problems2/question4.js
@@ -1,3 +1,6 @@
 let famousWords = "seven years ago...";
-let newStr = "Four score and " + famousWords;
+let startingWords = "Four score and";
+let newStr = startingWords + " " + famousWords;
+console.log(newStr);
+newStr = startingWords.concat(" ", famousWords);
 console.log(newStr);
